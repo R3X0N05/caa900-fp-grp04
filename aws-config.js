d9a7d2554abecadf21ff2b64da68890e3cf1ff33
Selva <selvaroshan@hotmail.com>
2026-06-19 12:06:27 -0400

    added cognito configs

--- aws-config.js
+++ aws-config.js
@@ -1,9 +1,10 @@
 // ─── AWS CONFIG ─────────────────────────────────────────────────
 const AWS_CONFIG = {
   API_BASE:      "https://9ok7xa70r0.execute-api.us-east-1.amazonaws.com/prod", // swap for API Gateway URL when on AWS
-  USER_POOL_ID:  "us-east-1_XXXXXXXXX",
-  CLIENT_ID:     "XXXXXXXXXXXXXXXXXXXXXXXXXX",
-  REGION:        "us-east-1",
-  COGNITO_DOMAIN:"https://rexony-auth.auth.us-east-1.amazoncognito.com",
+  USER_POOL_ID:  "us-east-1",
+  // CLIENT_ID:     "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+  COGNITO_REGION: "us-east-1",
+  COGNITO_USER_POOL_ID: "us-east-1_8ayr8j11B",
+  COGNITO_CLIENT_ID: "4s6kb6gqec66am6ejr24rg3bbl",
   STRIPE_PK:     "pk_test_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
