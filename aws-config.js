d0cf3ed0254cc708dce6f4b412edc7db73272bbb
Selva <selvaroshan@hotmail.com>
2026-06-19 14:57:27 -0400

    updated cognito config

--- aws-config.js
+++ aws-config.js
@@ -2,9 +2,9 @@
 const AWS_CONFIG = {
   API_BASE:             "https://9ok7xa70r0.execute-api.us-east-1.amazonaws.com/prod",
   COGNITO_REGION:       "us-east-1",
-  COGNITO_USER_POOL_ID: "us-east-1_uPrsJLvHn",
-  COGNITO_CLIENT_ID:    "2ub0t4gl0jneq70hs6mvvpjg7",
-  USER_POOL_ID:         "us-east-1_uPrsJLvHn",   // ← fixed (was "us-east-1")
-  CLIENT_ID:            "2ub0t4gl0jneq70hs6mvvpjg7",  // ← uncommented
+  COGNITO_USER_POOL_ID: "us-east-1_371y8nHWr",
+  COGNITO_CLIENT_ID:    "3hr3q5gntjsumupbi7jdgkinv5",
+  USER_POOL_ID:         "us-east-1_371y8nHWr",   // ← fixed (was "us-east-1")
+  CLIENT_ID:            "3hr3q5gntjsumupbi7jdgkinv5",  // ← uncommented
   STRIPE_PK:            "pk_test_XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 };
