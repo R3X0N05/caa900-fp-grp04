465a7ebd2a9a7bae6ea6c9b2a5aa691a6cb0e060
Selva Roshan Sivagnanasundaram Rexon <selvaroshan@hotmail.com>
2026-07-23 13:00:24 -0400

    test push to verify tf deployment

--- aws-config.js
+++ aws-config.js
@@ -1,10 +1,13 @@
 // ─── AWS CONFIG ─────────────────────────────────────────────────
 const AWS_CONFIG = {
-  API_BASE:             "https://9ok7xa70r0.execute-api.us-east-1.amazonaws.com/prod",
+  // API_BASE:             "https://9ok7xa70r0.execute-api.us-east-1.amazonaws.com/prod",
+  API_BASE: "https://bum2du8yy0.execute-api.us-east-1.amazonaws.com/prod",
   COGNITO_REGION:       "us-east-1",
   COGNITO_USER_POOL_ID: "us-east-1_Lw2Xpo4ll",
   COGNITO_CLIENT_ID:    "72p5tpp3hq15qae4hrap8lr0l5",
-  USER_POOL_ID:         "us-east-1_Lw2Xpo4ll",   // ← fixed (was "us-east-1")
-  CLIENT_ID:            "72p5tpp3hq15qae4hrap8lr0l5",  // ← uncommented
+  // USER_POOL_ID:         "us-east-1_Lw2Xpo4ll",   // ← fixed (was "us-east-1")
+  USER_POOL_ID: "us-east-1_oRrUTTzJw",
+  CLIENT_ID: "1vfq1oisto66pejbb15dkff6t6",
+  // CLIENT_ID:            "72p5tpp3hq15qae4hrap8lr0l5",  // ← uncommented
   STRIPE_PK:            "pk_test_51Tiw89RfsQqBbUtKpgkwHqAYuTGjrweeBg3Qrs3LK2Cny0mBrn02fIaboY96W0UdQ9PpOy5Yz0xKPGlTWE5sM3j000meASJl8i",
 };
