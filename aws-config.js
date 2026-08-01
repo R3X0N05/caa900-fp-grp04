f19871b413920873e30167491b36ad76b8cfe29c
Selva Roshan Sivagnanasundaram Rexon <selvaroshan@hotmail.com>
2026-08-01 15:26:36 -0400

    testing new infra tf

--- aws-config.js
+++ aws-config.js
@@ -1,10 +1,10 @@
 // ─── AWS CONFIG ─────────────────────────────────────────────────
 const AWS_CONFIG = {
-  API_BASE:             "https://9ok7xa70r0.execute-api.us-east-1.amazonaws.com/prod",
+  API_BASE:             "https://7ok07o8169.execute-api.us-east-1.amazonaws.com/prod",
   COGNITO_REGION:       "us-east-1",
-  COGNITO_USER_POOL_ID: "us-east-1_Lw2Xpo4ll",
-  COGNITO_CLIENT_ID:    "72p5tpp3hq15qae4hrap8lr0l5",
-  USER_POOL_ID:         "us-east-1_Lw2Xpo4ll",   // ← fixed (was "us-east-1")
-  CLIENT_ID:            "72p5tpp3hq15qae4hrap8lr0l5",  // ← uncommented
+  COGNITO_USER_POOL_ID: "us-east-1_7SlcFip44",
+  COGNITO_CLIENT_ID:    "2li78d9g9b231ha8okgu12qkk8",
+  USER_POOL_ID:         "us-east-1_7SlcFip44",
+  CLIENT_ID:            "2li78d9g9b231ha8okgu12qkk8",
   STRIPE_PK:            "pk_test_51Tiw89RfsQqBbUtKpgkwHqAYuTGjrweeBg3Qrs3LK2Cny0mBrn02fIaboY96W0UdQ9PpOy5Yz0xKPGlTWE5sM3j000meASJl8i",
 };
